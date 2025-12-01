9d961aed80ffe69bff5f6e02f71f6c7e2b61903a
Jaime <jaime1148@gmail.com>
2025-12-01 22:16:54 +0100

    Actualizo configuraciones y scripts SQL

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+-- CATEGORÍAS
+INSERT IGNORE INTO categorias (nombre, descripcion) VALUES
+('Tarjetas Gráficas', 'GPUs de alta gama para gaming y trabajo profesional'),
+('Procesadores', 'CPUs AMD e Intel de última generación'),
+('Placas Base', 'Motherboards compatibles con varios sockets'),
+('Periféricos', 'Teclados, ratones, auriculares y más'),
+('Almacenamiento', 'Discos SSD, NVMe y almacenamiento externo');
+
+-- PRODUCTOS
+INSERT IGNORE INTO productos (nombre, descripcion, precio, stock, categoria_id, imagen) VALUES
+('NVIDIA RTX 4070', 'Tarjeta gráfica de última generación con excelente rendimiento en 1440p', 699.99, 12, 1, 'https://thumb.pccomponentes.com/w-530-530/articles/1070/10706586/1500-gigabyte-geforce-rtx-4070-aero-oc-12gb-gddr6x-dlss3.jpg'),
+('AMD Ryzen 7 5800X', 'Procesador de 8 núcleos y 16 hilos ideal para gaming y productividad', 299.99, 18, 2, 'https://m.media-amazon.com/images/I/61DYLoyNRWL._AC_SL1500_.jpg'),
+('Intel Core i7-12700K', 'CPU híbrida con núcleos de rendimiento y eficiencia', 379.99, 10, 2, 'https://m.media-amazon.com/images/I/71iNHGS2w5L._AC_SL1500_.jpg'),
+('ASUS ROG Strix B550-F', 'Placa base de alto rendimiento AM4 para gaming', 189.99, 8, 3, 'https://thumb.pccomponentes.com/w-530-530/articles/32/325655/1580-asus-rog-strix-b550-f-gaming-wifi-ii.jpg'),
+('Teclado Mecánico RGB', 'Teclado mecánico con switches rojos y retroiluminación RGB', 89.99, 25, 4, 'https://m.media-amazon.com/images/I/71R+6aF8cOL._AC_SL1500_.jpg'),
+('SSD NVMe 1TB', 'Almacenamiento ultrarrápido PCIe 3.0 NVMe', 99.99, 30, 5, 'https://m.media-amazon.com/images/I/71vpl6RD4vL._AC_SL1500_.jpg');
